29721c3296d3feb8ed16509c32bb28d2ef6e41b6
BROTHER KA ADDA <ishvarkumarpachbiye@gmail.com>
2025-04-20 05:03:42 +0000

    It's still transaction not showing after refreshing page fix it

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -56,7 +56,7 @@ export default function TransactionsPage() {
       setName(parsedUserData.name);
       setUpiId(parsedUserData.upiId || '');
     }
-  }, []);
+  }, [personTransactions]);
 
   useEffect(() => {
     localStorage.setItem('personTransactions', JSON.stringify(personTransactions));
